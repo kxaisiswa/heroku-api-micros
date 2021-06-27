a468c2caddcbba62d34a93004fabe329dabe4ed6
kxaisiswa <kxairilazuan@gmail.com>
2021-06-27 21:43:17 +0800

    commit

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+const cors = require('cors');
+app.use(cors());
+
 const mysql = require('mysql');
 
 var con = mysql.createConnection({
