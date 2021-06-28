7cdec8cec686dd2e81c92cbf6a12660465bf8056
kxaisiswa <kxairilazuan@gmail.com>
2021-06-28 09:53:18 +0800

    commit

--- index.js
+++ index.js
@@ -24,5 +24,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(process.env.PORT || port, () => {
-  console.log('Example app listening to port ${port}.');
+  console.log('Example app listening to Heroku port.');
 });
